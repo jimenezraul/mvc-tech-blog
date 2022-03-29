7e787a58d4715d9c3987528e1aa1d1d114089c4f
Raul <jimenezraul1981@gmail.com>
2022-03-28 22:04:44 -0400

    add function to update post

--- public/javascript/update-delete-post.js
+++ public/javascript/update-delete-post.js
@@ -1,3 +1,5 @@
+// Delete post
+
 async function deletePost(event) {
   event.preventDefault();
   const postId = window.location.toString().split("/")[
@@ -16,4 +18,30 @@ async function deletePost(event) {
   }
 }
 
+// update post  
+async function updatePost(event) {
+    event.preventDefault();
+    const postId = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+    ];
+    const title = document.querySelector("#title").value.trim();
+    const content = document.querySelector("#content").value.trim();
+    const response = await fetch(`/api/posts/${postId}`, {
+        method: "put",
+        body: JSON.stringify({
+            title,
+            content,
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        alert(response.statusText);
+    }
+}
+
+
 document.querySelector("#delete-btn").addEventListener("click", deletePost);
+document.querySelector("#update-post-form").addEventListener("submit", updatePost);
